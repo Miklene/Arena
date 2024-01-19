93ec63dd928369260079d6a9d2c4a8be412a024b
Kotov Mikhail <mikhail.kotov@cybertech-signal.ru>
2024-01-19 15:56:47 +0300

    Добавил json для квестов. Поправил класс quest

--- world/quest.py
+++ world/quest.py
@@ -1,8 +1,9 @@
 class Quest:
-    def __init__(self, id: str, name: str, description: str) -> None:
+    def __init__(self, id: str, name: str, description: str, reward_exp: int) -> None:
         self.__id = id
         self.__name = name
         self.__description = description
+        self.__reward_exp = reward_exp
         self.__log = []
 
     def add_message_to_log(self, message: str):
